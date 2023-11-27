eab19847b94a6f19804bc2dfa11799ac924c64a8
Xiao Lin <linxiao.frank@gmail.com>
2023-11-26 21:04:08 -0500

    Deposit type classification v1.1 pipeline
    + Using LLMs for relevant paragraph filtering. Expect small accuracy improvement.
    + Restructure existing code
        - Paragraph and option filtering pushed to helper.py
        - Score calculation (temperature scaling) pushed to helper.py
    + Presents result better
        - Recording predictions, relevant paragraphs used and justification in ./sessions/
        - Included a set of example outputs in ./sessions/0000000

--- helper.py
+++ helper.py
@@ -4,7 +4,6 @@ from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM,AutoMod
 from transformers import pipeline
 
 
-
 def device_profile():
     n=torch.cuda.device_count()
     return [torch.cuda.get_device_properties(i).total_memory for i in range(n)]
@@ -148,54 +147,153 @@ def perplexity2(model,tokenizer,text,option,IGNORE_INDEX=-100):
     return loss_q,loss_a
 
 
-def perplexity2b(model,tokenizer,text,option,IGNORE_INDEX=-100):
+#Perplexity for 2 sentences concatenated, but with tokens
+def perplexity2_tokens(model,tokens0,tokens1,IGNORE_INDEX=-100):
     with torch.no_grad():
-        x=tokenizer(text,return_tensors='pt',padding='longest',max_length=tokenizer.model_max_length)
-        z=tokenizer(option,return_tensors='pt',padding='longest',max_length=tokenizer.model_max_length)
-        
-        #Try to cut tokens to the length of just the text
-        s=tokenizer.decode(z['input_ids'][0])
-        if not s==option and s.find(option)>0: #Check if there's bos padding
-            z={k:v[:,1:] for k,v in z.items()}
-            s=tokenizer.decode(z['input_ids'][0])
-        
-        if not s==option and s.find(option)==0: #Check if there's eos padding
-            z={k:v[:,:-1] for k,v in z.items()}
-            x={k:v[:,:-1] for k,v in x.items()}
-            s=tokenizer.decode(z['input_ids'][0])
-        
-        if not s==option: #Not sure what's going on
-            print('Warning',s,option,z)
-            a=0/0
-        
-        #Synthesize input/output with option
-        #Append option after input
-        xz={k:torch.cat((x[k],z[k]),dim=-1) for k,v in x.items()}
-        if 'token_type_ids' in xz:
-            _=xz.pop('token_type_ids')
-        
-        xz={k:v.to(model.device) for k,v in xz.items()}
-        
-        y=xz['input_ids'].data.clone()
-        y[:,:-1]=y[:,1:].clone()
-        y[:,-1]=IGNORE_INDEX
-        
-        pred=model(**xz,labels=y)
-        #Check CE
-        logp=F.log_softmax(pred.logits.squeeze(0),dim=-1)
-        logp=logp.gather(1,y.clamp(min=0).view(-1,1)).view(-1)
-        
-        loss_q=logp[:x['input_ids'].shape[-1]-1].clone()
-        loss_a=logp[x['input_ids'].shape[-1]-1:-1].clone()
+        tokens0=torch.LongTensor(tokens0).cuda()
+        tokens1=torch.LongTensor(tokens1).cuda()
+        input_ids=torch.cat((tokens0,tokens1),dim=-1)
+        logp=model(input_ids)['logits']
+        logp=F.log_softmax(logp,dim=-1)
+        logp=logp[:,:-1,:].gather(-1,input_ids[:,1:].unsqueeze(-1)).sum(-1)
+        logp0=logp[:,:tokens0.shape[-1]-1].clone().cpu()
+        logp1=logp[:,tokens0.shape[-1]-1:].clone().cpu()
     
-    return loss_q,loss_a
+    return logp0,logp1
+
+#Use when tokens0 remains being the same thing throughout
+#Use_cache to accelerate perplexity computation
+def perplexity2_tokens_cache(model,tokens0,tokens1,IGNORE_INDEX=-100,bsz=5):
+    with torch.no_grad():
+        t=tokens0[-1]
+        tokens0=torch.LongTensor(tokens0[:-1]).unsqueeze(0).cuda()
+        tokens1=torch.LongTensor([[t]+x for x in tokens1]).cuda()
+        cache=model(tokens0,use_cache=True)['past_key_values']
+        logp1=[]
+        for i in range(0,len(tokens1),bsz):
+            r=min(i+bsz,len(tokens1))
+            #Make bigger cache to match tokens1
+            cache_i=[[z.repeat(r-i,1,1,1) for z in x] for x in cache]
+            logp_i=model(tokens1[i:r],past_key_values=cache_i)['logits']
+            logp_i=F.log_softmax(logp_i,dim=-1)
+            logp1_i=logp_i[:,:-1,:].gather(-1,tokens1[i:r,1:].unsqueeze(-1)).sum(-1).cpu()
+            logp1.append(logp1_i)
+        
+        logp1=torch.cat(logp1,dim=0)
+    return logp1
+
+#Return text does not have BOS
+def filter_lm(model,tokenizer,document,prompt='',topk=50,L=200,bsz=5):
+    #Works only for the LLaMA family
+    bos=tokenizer('a.')['input_ids'][0]
+    stop=tokenizer('a.')['input_ids'][-1]
+    #Find appropriate places for inserting this sentence
+    all_tokens=tokenizer(document)['input_ids'][1:]
+    q=tokenizer(prompt)['input_ids'][1:]
+    
+    N=L
+    step=L//2
+    
+    #Find possible places for inserting the sentence
+    stops=[i for i,t in enumerate(all_tokens) if t==stop]
+    tprev=0
+    stops2=[]
+    for t in stops:
+        if t>=N-1 and t-tprev>=step:
+            s=min([i for i in stops if i>=t-N+1])
+            #stops2.append((s+1,t+1))
+            stops2.append((t-N+1,t+1))
+            tprev=t
+    
+    data=[[bos]+all_tokens[s:t] for s,t in stops2]
+    #print(stops2)
+    
+    #Batch inferencing to cap GPU util
+    #Leverages the fact that all chunks are the same num.tokens
+    batch=bsz
+    scores=[]
+    for i in range(0,len(data),batch):
+        print('filter %d/%d   '%(i,len(data)),end='\r')
+        data_i=data[i:i+batch]
+        _,logp=perplexity2_tokens(model,data_i,[q for i in range(len(data_i))])
+        scores.append(logp.mean(dim=-1))
+    
+    scores=torch.cat(scores,dim=-1)
+    _,ind=scores.sort(dim=0,descending=True)
+    pos=ind[:topk].tolist()
+    
+    out=[]
+    for i in pos:
+        s,t=stops2[i]
+        #Move the start position to a full stop
+        #Leave the end position dangling
+        s=min([i for i in stops if i>=t-N+1])
+        if s+N+1>len(all_tokens):
+            s=max([i for i in stops if i+N+1<=len(all_tokens)])
+        
+        out.append((s+1,s+N+1))
+    
+    doc=[all_tokens[x[0]:x[1]] for x in sorted(out,key=lambda x:x[0])]
+    out=[all_tokens[x[0]:x[1]] for x in out]
+    return out,doc #[data[i][1:] for i in pos] if we want start position dangling
+
+#Prefix-based multiple-choice
+def prefix_mc(model,paras,options,T=0.01,bsz=5):
+    scores=[]
+    for i,o in enumerate(options):
+        s=perplexity2_tokens_cache(model,o,paras,bsz=bsz)
+        scores.append(s.mean(dim=-1))
+        print('Prefix MC %d/%d'%(i,len(options)),end='\r')
+    
+    scores=torch.cat(scores,dim=0).view(len(options),len(paras)).t().contiguous()
+    scores_avg=F.softmax(scores/T,dim=-1).mean(dim=0)
+    return scores_avg,scores
+
+#Retrieval + prefix-based multiple-choice
+def RAPMC(model,tokenizer,document,options,retrieval_prompt,params=None):
+    import util.smartparse as smartparse
+    default_params=smartparse.obj()
+    default_params.topk=20
+    default_params.L=200
+    default_params.T=0.01
+    default_params.bsz=5
+    default_params.short_list=True
+    default_params.topn=50
+    default_params.topn_nparas=5
+    params = smartparse.merge(params, default_params)
+    
+    #Retrieve relevant paragraphs (in tokens)
+    paras,doc=filter_lm(model,tokenizer,document,prompt=retrieval_prompt,topk=params.topk,L=params.L,bsz=params.bsz)
+    
+    #Run question answering on top 5 paragraphs to narrow down options
+    options_tok=[tokenizer(o)['input_ids'] for o in options]
+    if params.short_list:
+        scores,_=prefix_mc(model,paras[:params.topn_nparas],options_tok,T=params.T,bsz=params.bsz)
+        _,ind=scores.sort(dim=-1,descending=True)
+        ind=ind[:params.topn].tolist()
+    else:
+        ind=list(range(len(options_tok)))
+    
+    options_tok_short=[options_tok[i] for i in ind]
+    
+    #Run full question answering on short list
+    scores_short,scores_short_raw=prefix_mc(model,paras,options_tok_short,T=params.T,bsz=params.bsz)
+    
+    #Map short list probs to original list
+    scores=torch.Tensor(len(options)).fill_(0)
+    scores[ind]=scores_short
+    
+    scores_raw=torch.Tensor(len(paras),len(options)).fill_(-1e10)
+    scores_raw[:,ind]=scores_short_raw
+    return scores,paras,doc,scores_raw
+
 
 
 #Generation
 def generate(model,tokenizer,text,max_new_tokens=500,**kwargs):
     with torch.no_grad():
         x=tokenizer(text,return_tensors='pt',padding='longest',max_length=tokenizer.model_max_length)
-        y=model.generate(input_ids=x['input_ids'].cuda(),max_new_tokens=500,**kwargs)
+        y=model.generate(input_ids=x['input_ids'].cuda(),max_new_tokens=max_new_tokens,**kwargs)
         out=tokenizer.decode(y[0].tolist())
         out2=tokenizer.decode(y[0,x['input_ids'].shape[-1]:].tolist())
     
--- run.py
+++ run.py
@@ -19,12 +19,6 @@ pip install accelerate
 pip install backoff
 
 '''
-import backoff 
-import openai 
-@backoff.on_exception(backoff.expo, openai.error.RateLimitError)
-def completions_with_backoff(**kwargs):
-    return openai.ChatCompletion.create(**kwargs)
-
 import time
 import os
 import gc
@@ -36,20 +30,14 @@ import json
 
 import torch
 import torch.nn.functional as F
-import torch
 import torch.nn as nn
-import torch.optim as optim
 
-import util.smartparse as smartparse
-import util.session_manager as session_manager
-import torch
-import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM,AutoModel
-from transformers import pipeline
-
 
 import helper
 import ocr
+import util.smartparse as smartparse
+import util.session_manager as session_manager
 
 
 def default_params():
@@ -59,10 +47,14 @@ def default_params():
     params.lm='NousResearch/Llama-2-7b-hf'
     #params.lm='openlm-research/open_llama_3b_v2'
     params.openai_key=''
-    params.topk=50
     params.style='v0'
-    params.chunk=300
-    params.T=1e-2
+    
+    params.topk=20
+    params.L=200
+    params.topn=50
+    params.topn_nparas=5
+    params.T=0.01
+    params.bsz=5
     return params
 
 params = smartparse.parse()
@@ -72,14 +64,24 @@ session=session_manager.create_session(params);
 
 t0=time.time()
 
-data=ocr.ocr_parallel(params.pdf)
+#OCR the incoming document
+session.log('%s'%params.pdf)
+session.log('')
+data=ocr.ocr_parallel(params.pdf,num_workers=16)
+with open(session.file('ocr.json'),'w') as f:
+    json.dump(data,f)
+
 print('OCR time %.2f'%(time.time()-t0))
+doc=''.join(data)
 
 
-model_embed,tokenizer_embed=helper.load_embedding('BAAI/bge-large-en-v1.5')
-model,tokenizer=helper.load_lm(params.lm,dtype=torch.half)
-print('Load LLMs time %.2f'%(time.time()-t0))
-
+#Prepare deposit type options
+def clean_str(s):
+    s=s.replace('\n','')
+    s=s.replace('  ',' ')
+    s=s.replace('- ','-')
+    s=s.replace(' -','-')
+    return s
 
 labels=pandas.read_csv(params.options)
 options=list(labels['label'])
@@ -88,120 +90,57 @@ print('Load labels time %.2f'%(time.time()-t0))
 
 #Prepare QA templates
 style=params.style
-if style=='v2':
-    q_template='DEPOSIT MODEL OF {option}\n{option} deposits have been spotted in the example below. {option_aug}\n\nEXAMPLE OF {option} deposit\n"...'
-    a_template='{para}'
-    sep='</s><s>'
-elif style=='v1':
-    q_template='DEPOSIT MODEL OF {option}\n{option} deposits have been spotted in the example below. {option_aug}\n\nEXAMPLE OF {option} deposit\n"...'
-    a_template='{para}'
-    sep='\n\n'
-elif style=='v0':
-    q_template='This area has been identified as a {option} deposit. {option_aug}\n\n...'
-    a_template='{para}'
-    sep='\n\n'
+if style=='v0':
+    options_desc=['This area has been identified as a %s deposit. %s\n\n...'%(clean_str(options[i]),clean_str(options_aug[i])) for i in range(len(options))]
 elif style=='title':
-    q_template='This area has been identified as a {option} deposit. \n\n...'
-    a_template='{para}'
-    sep='\n\n'
-elif style=='qa':
-    q_template='Answer the yes/no question based on the context below with only "Yes" or "No", or "Unsure" if the context does not provide enough information.\n\nContext: ...{para}...\n\nQuestion: Is the mineral deposit in this area a {option} deposit?\n\n Answer:'
-    a_template='Yes'
-    sep='\n\n'
-elif style=='qav2':
-    q_template=helper.qa_template('Context: ...{para}...\n\nQuestion: Is the mineral deposit in this area a {option} deposit?',sys='You are a friendly geologist chatbot who always responds to user questions accurately. Answer the yes/no question based on the context below with only "Yes" or "No", or "Unsure" if the context does not provide enough information.',style='zephyr')
-    a_template='Yes'
-    sep='\n\n'
-elif style=='qa_aug':
-    q_template='Answer the yes/no question based on the context below with only "Yes" or "No", or "Unsure" if the context does not provide enough information.\n\nContext: ...{para}...\n\nQuestion: Is the mineral deposit in this area a {option} deposit? {option_aug}\n\n Answer:'
-    a_template='Yes'
-    sep='\n\n'
-else:
-    a=0/0
+    options_desc=['This area has been identified as a %s deposit.\n\n...'%(clean_str(options[i])) for i in range(len(options))]
 
-def qa_few_shot(q_template,a_template,sep,examples):
-    q=''
-    a=''
-    for i,ex in enumerate(examples):
-        qi=q_template.format(**ex)
-        ai=a_template.format(**ex)
-        q+=qi
-        if i<len(examples)-1:
-            q+=ai
-            q+=sep
-        else:
-            a+=ai
-    
-    return q,a
+retrieval_prompt='With these information, researchers believe that the mineral deposit in this area follows the type of'
 
 
+#Load LLM
+model,tokenizer=helper.load_lm(params.lm,dtype=torch.half)
+print('Load LLMs time %.2f'%(time.time()-t0))
 
-
-s=params.chunk
-all_data=''.join(data)
-data_chunks=[all_data[i:i+s] for i in range(0,len(all_data),s//2)]
-
-#Filter text, return top 10 most relevant chunks
-ref='Detailed studies reveal that this area contains a %s deposit.'
-ref=[ref%o for o in options]
-data2,s_embed=helper.filter_embedding(model_embed,tokenizer_embed,data_chunks,ref,topk=50)
-print('Retrieve relevant paragraphs time %.2f'%(time.time()-t0))
-
-s=F.softmax(s_embed/params.T,dim=-1).mean(dim=0)
-s,ind=s.sort(dim=-1,descending=True)
-options=[options[i] for i in ind[:50].tolist()]
-options_aug=[options_aug[i] for i in ind[:50].tolist()]
-print('Subselect options time %.2f'%(time.time()-t0))
-data2=data2[:params.topk]
-
-#Scores
-scores=[]
-for i,para in enumerate(data2):
-    for j,o in enumerate(options):
-        q,a=qa_few_shot(q_template,a_template,sep,[{'para':para,'option':o,'option_aug':options_aug[j]}])
-        s0,s1=helper.perplexity2(model,tokenizer,q,a)
-        scores.append(float(s1.mean()))
-        print('QA %d/%d, %d/%d    %f, time %.2f '%(i,len(data2),j,len(options),s1.mean(),time.time()-t0),end='\r')
-
-scores=torch.Tensor(scores).view(len(data2),len(options))
-s=F.softmax(scores/params.T,dim=-1).mean(dim=0)
-print('QA scores time %.2f'%(time.time()-t0))
-
-torch.save(s,'scores.pt')
-s,ind=s.sort(dim=-1,descending=True)
+#Perform QA
+scores,paras,doc_short,scores_raw=helper.RAPMC(model,tokenizer,doc,options_desc,retrieval_prompt,params)
+s,ind=scores.sort(dim=-1,descending=True)
 ind=ind.tolist()
 for i in range(10):
     o=options[int(ind[i])]
-    print('Rank %d\t%.4f\t%s'%(i+1,s[i],o.replace('\n','')))
+    session.log('Rank %d\t%.4f\t%s'%(i+1,s[i],o.replace('\n','')))
 
+session.log('')
+df=pandas.DataFrame({'label':[options[ind[i]] for i in range(len(ind))],'score':s.tolist()})
+df.to_csv(session.file('predictions.csv'))
 
-print('')
+#Check top relevant paragraphs
+scores_raw=F.softmax(scores_raw/params.T,dim=-1)
+s_para,ind_para=scores_raw[:,ind[0]].sort(dim=0,descending=True)
+df=pandas.DataFrame({'paragraph':[tokenizer.decode(paras[ind_para[i]]) for i in range(len(ind_para))],'score':s_para.tolist()})
+df.to_csv(session.file('relevant_paragraphs.csv'))
+print('Prediction time %.2f'%(time.time()-t0))
 
-#Organize chunks
-def concat(data2,data):
-    mark=[]
-    for j in range(30):
-        ind=data.index(data2[j])
-        mark.append(max(ind-1,0))
-        mark.append(ind)
-        mark.append(min(ind+1,len(data2)))
-    
-    mark=sorted(list(set(mark)))
-    
-    para=''
-    prev=-1
-    for j in mark:
-        if j==prev+1:
-            para+=data[j]
-        else:
-            para+='...'+data[j]
-    
-    return para
-
-msg0=[{"role": "system", "content": "You are a helpful assistant specialized in reviewing geological publications and answering questions about them. Please read the context document and answer the multiple-choice question about the context, and show your reasoning."}]
-msg='Context: %s\n Question: does this document mention any of the following deposits? A.%s. %s\nB.%s. %s\nC.%s. %s\nD.%s. %s\nE.%s. %s\n'%(concat(data2,data_chunks),*[x.replace('\n','') for x in [options[ind[0]],options_aug[ind[0]],options[ind[1]],options_aug[ind[1]],options[ind[2]],options_aug[ind[2]],options[ind[3]],options_aug[ind[3]],options[ind[4]],options_aug[ind[4]]]])
-
+#Generate explanations
 if not params.openai_key=='':
+    doc_short='\n\n...'.join([tokenizer.decode(x) for x in doc_short])
+    import backoff 
+    import openai 
+    @backoff.on_exception(backoff.expo, openai.error.RateLimitError)
+    def completions_with_backoff(**kwargs):
+        return openai.ChatCompletion.create(**kwargs)
+    
+    options_gpt=['%s. %s'%(clean_str(options[ind[i]]),clean_str(options_aug[ind[i]])) for i in range(5)]
+    msg0=[{"role": "system", "content": "You are a helpful assistant specialized in reviewing geological publications and answering questions about them. Please read the context document and answer the multiple-choice question about the context. There is only one option that is correct. Start your answer with which option you'd like to pick, before showing your reasoning."}]
+    msg='Context: %s\nWhich of the following mineral deposit types best fits the area that the document describes?\nA.%s\nB.%s\nC.%s\nD.%s\nE.%s\n'%(doc_short,*[clean_str(x) for x in [options_gpt[i] for i in range(5)]])
+    msg=msg0+[{"role": "user", "content": msg}]
+    
     openai.api_key = params.openai_key
-    completion = completions_with_backoff(model="gpt-4", messages=msg0+[{"role": "user", "content": msg}])
-    print('GPT-4 justification: %s'%completion.choices[0].message.content)
+    completion = completions_with_backoff(model="gpt-4-1106-preview", messages=msg)
+    session.log('GPT-4 justification')
+    session.log('SYSTEM: %s'%msg[0]['content'][:300])
+    session.log('USER: %s'%msg[1]['content'][:300])
+    session.log('ASSISTANT: %s'%completion.choices[0].message.content)
+    with open(session.file('explanation.json'),'w') as f:
+        json.dump({'explanation':completion.choices[0].message.content},f)
+    
